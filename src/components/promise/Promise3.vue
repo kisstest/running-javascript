8adef97c9f806d4333e3ea7ef298443060a90783
kisstest <kisstest2@gmail.com>
2018-08-11 18:51:15 +0900

    fix error

--- src/components/promise/Promise3.vue
+++ src/components/promise/Promise3.vue
@@ -14,72 +14,77 @@ export default {
   data() {
     return {
       result: '주문이 아직 없습니다.',
-      /**
-       * 생두 저장소 객체
-       * @namespace
-       */
-      beansStorage: {
-        /**
-         * 메뉴에 해당하는 생두를 반환한다.
-         * @param {string} menu
-         * @returns {Promise}
-         */
-        get(type) {
-          return new Promise((resolve, reject) => {
-            if (type === '케냐') resolve('생두');
-            else reject(new Error('알 수 없는 타입입니다.'));
-          });
-        },
-      },
-      /**
-       * 로스터기 객체
-       * @namespace
-       */
-      roaster: {
-        /**
-         * 생두를 로스팅하여 원두를 반환한다.
-         * @param {string} greenBeans
-         * @returns {Promise}
-         */
-        excute(beanType) {
-          return this.beansStorage
-            .get(beanType)
-            .then((greenBeans) => {
-              return new Promise((resolve, reject) => {
-                if (greenBeans === '생두') resolve('원두');
-                else reject(new Error('생두가 아닙니다.'));
-              });
-            });
-        },
-      },
-      /**
-       * 커피 하우스 객체
-       */
-      coffeehouse: {
-        /**
-         * 커피를 주문한다.
-         * @param {string} menu
-         * @returns {Promise}
-         */
-        order(menu) {
-          return this.roaster
-            .excute('케냐')
-            .then((blackBeans) => {
-              return new Promise((resolve, reject) => {
-                if (menu === '아메리카노' && blackBeans === '원두') resolve('맛있는 아메리카노');
-                else reject(new Error('알 수 없는 타입입니다.'));
-              });
-            });
-        },
-      },
     };
   },
   components: { HomeButton },
   methods: {},
   computed: {},
-  mouted() {
+  mounted() {
+    console.log('nextTick1');
+    /**
+     * 생두 저장소 객체
+     * @namespace
+     */
+    const beansStorage = {
+      /**
+       * 메뉴에 해당하는 생두를 반환한다.
+       * @param {string} menu
+       * @returns {Promise}
+       */
+      get(type) {
+        return new Promise((resolve, reject) => {
+          if (type === '케냐') resolve('생두');
+          else reject(new Error('알 수 없는 타입입니다.'));
+        });
+      },
+    };
+    /**
+     * 로스터기 객체
+     * @namespace
+     */
+    const roaster = {
+      /**
+       * 생두를 로스팅하여 원두를 반환한다.
+       * @param {string} greenBeans
+       * @returns {Promise}
+       */
+      excute(beanType) {
+        return beansStorage
+          .get(beanType)
+          .then((greenBeans) => {
+            const promise = new Promise((resolve, reject) => {
+              if (greenBeans === '생두') resolve('원두');
+              else reject(new Error('생두가 아닙니다.'));
+            });
+            return promise;
+          });
+      },
+    };
+    /**
+     * 커피 하우스 객체
+     */
+    const coffeehouse = {
+      /**
+       * 커피를 주문한다.
+       * @param {string} menu
+       * @returns {Promise}
+       */
+      order(menu) {
+        return roaster
+          .excute('케냐')
+          .then((blackBeans) => {
+            const promise = new Promise((resolve, reject) => {
+              if (menu === '아메리카노' && blackBeans === '원두') resolve('맛있는 아메리카노');
+              else reject(new Error('알 수 없는 타입입니다.'));
+            });
+            return promise;
+          });
+      },
+    };
+
     this.$nextTick(() => {
-      this.coffeehouse
+      console.log('nextTick2');
+      coffeehouse
         .order('아메리카노')
         .then((coffee) => {
           this.result = `${coffee}가 나왔습니다.`;
