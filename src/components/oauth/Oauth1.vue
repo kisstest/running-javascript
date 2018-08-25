987af4cbc62b05f07fdaa96aa43b42fc15e1f480
kisstest <kisstest2@gmail.com>
2018-08-25 18:51:40 +0900

    ...

--- src/components/oauth/Oauth1.vue
+++ src/components/oauth/Oauth1.vue
@@ -3,7 +3,7 @@
     <home-button></home-button>
     <h1>Oauth1</h1>
     <button type="button" class="btn btn-primary"
-      @click="oauth"
+      @click="initClient"
     >Oauth 시작하기</button>
   </div>
 </template>
@@ -12,97 +12,31 @@
 import HomeButton from '@/components/common/HomeButton';
 
 export default {
+  name: 'Oauth1',
   data() {
     return {
       googleAuth: null,
-      scope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly',
     };
   },
   components: { HomeButton },
   methods: {
-    oauth() {
+    initClient() {
+      window.gapi.auth2.init({
+        apiKey: 'AIzaSyD73vQfROYv3rr8__SU8r7ZzyEKYPpeOmU',
+        clientId: '809257081995-at1j9ket616el274c69p4iffjf3n1ldn.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly',
+      }).then(() => {
+        this.googleAuth = gapi.auth2.getAuthInstance();
+        // Listen for sign-in state changes.
+        // this.googleAuth.isSignedIn.listen(updateSigninStatus);
+      });
     },
   },
   mounted() {
-    function handleClientLoad() {
-      // Load the API's client and auth2 modules.
-      // Call the initClient function after the modules load.
-      gapi.load('client:auth2', initClient);
-    }
-
-    function initClient() {
-      // Retrieve the discovery document for version 3 of Google Drive API.
-      // In practice, your app can retrieve one or more discovery documents.
-      // var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
-
-      // Initialize the gapi.client object, which app uses to make API requests.
-      // Get API key and client ID from API Console.
-      // 'scope' field specifies space-delimited list of access scopes.
-      gapi.client.init({
-          // 'apiKey': 'YOUR_API_KEY',
-          // 'discoveryDocs': [discoveryUrl],
-          'clientId': 'YOUR_CLIENT_ID',
-          'scope': this.scope,
-      }).then(function () {
-        this.googleAuth = gapi.auth2.getAuthInstance();
-
-        // Listen for sign-in state changes.
-        this.googleAuth.isSignedIn.listen(updateSigninStatus);
-
-        // Handle initial sign-in state. (Determine if user is already signed in.)
-        var user = this.googleAuth.currentUser.get();
-        setSigninStatus();
-
-        // Call handleAuthClick function when user clicks on
-        //      "Sign In/Authorize" button.
-        $('#sign-in-or-out-button').click(function() {
-          handleAuthClick();
-        }); 
-        $('#revoke-access-button').click(function() {
-          revokeAccess();
-        }); 
-      });
-    }
-
-    function handleAuthClick() {
-      if (this.googleAuth.isSignedIn.get()) {
-        // User is authorized and has clicked 'Sign out' button.
-        this.googleAuth.signOut();
-      } else {
-        // User is not signed in. Start Google auth flow.
-        this.googleAuth.signIn();
-      }
-    }
-
-    function revokeAccess() {
-      this.googleAuth.disconnect();
-    }
-
-    function setSigninStatus(isSignedIn) {
-      var user = this.googleAuth.currentUser.get();
-      var isAuthorized = user.hasGrantedScopes(this.scope);
-      if (isAuthorized) {
-        $('#sign-in-or-out-button').html('Sign out');
-        $('#revoke-access-button').css('display', 'inline-block');
-        $('#auth-status').html('You are currently signed in and have granted ' +
-            'access to this app.');
-      } else {
-        $('#sign-in-or-out-button').html('Sign In/Authorize');
-        $('#revoke-access-button').css('display', 'none');
-        $('#auth-status').html('You have not authorized this app or you are ' +
-            'signed out.');
-      }
-    }
-
-    function updateSigninStatus(isSignedIn) {
-      setSigninStatus();
-    }
   },
 };
 </script>
-https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.readonly
 
-AIzaSyDYx8f4_fhYw2GP9LTxVoArwopuNYf8Dk0
 <style lang="scss" scoped>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'bootstrap';
+import GAuth from 'vue-google-oauth2';
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
@@ -9,6 +10,10 @@ import router from './router';
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
+Vue.use(GAuth, {
+  clientId: '809257081995-at1j9ket616el274c69p4iffjf3n1ldn.apps.googleusercontent.com',
+  scope: 'profile email https://www.googleapis.com/auth/plus.login',
+});
 
 /* eslint-disable no-new */
 new Vue({
